60a9e2570092ab5a9c67940bccd9bf1df79e66a8
MOHAMED <aboelftohm1@gmail.com>
2023-08-19 16:33:56 +0300

    +Part Of Hadeth

--- app/src/main/java/com/islamy_mohamed/ui/Home_Screen/Fragments/HadethFragment.kt
+++ app/src/main/java/com/islamy_mohamed/ui/Home_Screen/Fragments/HadethFragment.kt
@@ -1,12 +1,19 @@
 package com.islamy_mohamed.ui.Home_Screen.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.islamy_mohamed.R
+import com.islamy_mohamed.ui.activityHadethDetails.HadethDetailsActivity
+
 class HadethFragment : Fragment() {
+    lateinit var RvHadeth : RecyclerView
+     var listHadeth = ArrayList<String>()
+    lateinit var adapterHadeth : AdapterForRvHadethFragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -15,4 +22,31 @@ class HadethFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_hadeth, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        RvHadeth = view.findViewById(R.id.RV_Hadeth)
+        makeRvHadethWorked()
+       adapterHadeth.onItemClickListener = object : AdapterForRvHadethFragment.OnItemClickListener{
+           override fun OnItemClick(position: Int, name: String) {
+               val intent = Intent(context,HadethDetailsActivity::class.java)
+               intent.putExtra("index",position)
+               intent.putExtra("hadethName",name)
+               startActivity(intent)
+           }
+
+       }
+
+    }
+fun initList(){
+    for(i in 0 .. 49)
+    {
+        listHadeth.add("حديث رقم ${i+1}")
+    }
+}
+    fun makeRvHadethWorked(){
+        initList()
+        adapterHadeth = AdapterForRvHadethFragment(listHadeth)
+        RvHadeth.adapter=adapterHadeth
+    }
 }
